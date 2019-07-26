b6b7a6b2b64845501faabe93e56bbae4e0533713
Zezzman <zezzman97@gmail.com>
2019-07-27 01:14:43 +1000

    cli route default

--- routes/cli.php
+++ routes/cli.php
@@ -1,3 +1,3 @@
 <?php
+// $provider->command(command, [params], 'Controller@Action');
 $provider->default('Home@Index');
-$provider->command('--database', ['username', 'password'], 'Database@Login');
