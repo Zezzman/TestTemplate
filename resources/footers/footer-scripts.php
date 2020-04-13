2c056bc72d91481a4ac3b652e67f5a3bdbf07a2b
Zezzman <francoisleroux97@gmail.com>
2020-04-13 13:36:08 +1000

    footer

--- resources/footers/footer-scripts.php
+++ resources/footers/footer-scripts.php
@@ -1,10 +1,5 @@
-<?php $plugins = config('LINKS.PUBLIC~PLUGINS'); ?>
-<script src="<?= $plugins . 'node_modules\jquery\dist\jquery.min.js'; ?>"></script>
-<script src="<?= $plugins . 'node_modules\jquery-validation\dist\jquery.validate.js'; ?>"></script>
-<script src="<?= $plugins . 'node_modules\popper.js\dist\umd\popper.min.js'; ?>"></script>
-<script src="<?= $plugins . 'node_modules\bootstrap\dist\js\bootstrap.min.js'; ?>"></script>
-<?php
-echo System\Helpers\FileHelper::loadScripts((array) config('LAYOUT.FOOTER.SCRIPTS'));
+<?php 
+echo \System\Helpers\FileHelper::loadScripts((array) config('LAYOUT.FOOTER.SCRIPTS'));
 // Print appending scripts
 echo \System\Helpers\FileHelper::loadScripts($viewData->bag['scripts'] ?? []);
 ?>
