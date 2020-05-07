0eef8611900481a6ce98420700ef3b078f16e6fc
Zezzman <francoisleroux97@gmail.com>
2020-05-08 02:18:08 +1000

    layout css changes

--- public/assets/javascript/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // disable form submit
-    $('.no-submit').submit(false);
-    // enable tooltips on data-toggle=tooltip
-    $('[data-toggle="tooltip"]').tooltip();
-    // $('[data-tooltip]').tooltip('show');
-});
